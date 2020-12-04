1b00e4c23615e7a35767b3606931d578b39d8cee
Dean Turner <dean@manektech.com>
2020-12-04 14:58:13 +0530

    added testing

--- test/api.test.js
+++ test/api.test.js
@@ -1,65 +1,45 @@
 const request = require("supertest")("http://localhost:3000");
 const expect = require("chai").expect;
 
-describe("GET /blog", function() {
-    it("returns all blogs", async function() {
-        const response = await request.get("/blog");
+describe("GET /purchase/List", function() {
+    it("returns all purchases", async function() {
+        const response = await request.get("/purchase/List");
         expect(response.status).to.eql(200);
     });
 });
 
-describe("POST /blog", function() {
-    it("create blogs", async function() {
+describe("POST /purchase/create", function() {
+    it("create purchase", async function() {
         const response = await request
-            .post("/blog")
+            .post("/purchase/create")
             .send({
-                "nickname": "Article TWo",
-                "title": "Article second",
-                "content": "this is test article",
-                "created_date": "2020-12-02 17:57:47"
+                "brand": "Article TWo",
+                "amount": "200",
+                "type": "Grocery",
+                "location": "Dubai"
             });
         expect(response.status).to.eql(200);
     });
 });
 
-describe("GET /blog/:id", function() {
-    it("get single blog", async function() {
-        const response = await request.get("/blog/4"); // here 4 is blog id
-        expect(response.status).to.eql(200);
-    });
-});
-
-describe("GET /comment/:articleId", function() {
-    it("get single blog", async function() {
-        const response = await request.get("/blog/5"); // here 4 is blog id
-        expect(response.status).to.eql(200);
-    });
-});
-
-describe("POST /comment", function() {
-    it("create comment", async function() {
+describe("POST /purchase/create", function() {
+    it("update purchase", async function() {
         const response = await request
-            .post("/comment")
+            .post("/purchase/create")
             .send({
-                "nickname": "Nice",
-                "content": "atricle",
-                "blog_id": "5",
-                "created_date": "2020-12-02 17:57:47"
+                "brand": "Article TWo",
+                "amount": "200",
+                "type": "Grocery",
+                "location": "Dubai",
+                "_id": "5fc9edb30a73d41bb43dadca" // purchase id
             });
         expect(response.status).to.eql(200);
     });
 });
 
-describe("POST /comment/Reply", function() {
-    it("comment on a comment", async function() {
-        const response = await request
-            .post("/comment/Reply")
-            .send({
-                "nickname": "comment",
-                "content": "comment",
-                "comment_id": "4",
-                "created_date": "2020-12-02 17:57:47"
-            });
+describe("GET /Item/List", function() {
+    it("returns all purchases", async function() {
+        const response = await request.get("/Item/List");
         expect(response.status).to.eql(200);
     });
 });
